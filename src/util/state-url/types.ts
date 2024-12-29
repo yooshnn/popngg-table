ab31a1ea278d684fbd9dfe775559bbecaa694b43
yooshnn <yooshnn@kakao.com>
2024-12-29 13:48:27 +0900

    refactor: move reducer-url to plugins folder

--- src/util/state-url/types.ts
+++
@@ -1,7 +0,0 @@
-export type EncodableValue = string | number | Array<string> | Array<number>;
-export type EncodableState = Record<string, EncodableValue>;
-
-export type GetStateUrlOption = Record<string, (value: string) => unknown>;
-export type GetStateUrlReturn<Request extends GetStateUrlOption> = {
-  [Key in keyof Request]: ReturnType<Request[Key]> | undefined;
-};
